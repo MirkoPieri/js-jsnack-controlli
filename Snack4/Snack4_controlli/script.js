d6af6d509adfee3bf193c808a7703e99df306d86
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-05 17:03:38 +0200

    Controllo inserimento input.

--- Snack4/Snack4_controlli/script.js
+++ Snack4/Snack4_controlli/script.js
@@ -8,17 +8,30 @@ var elemento = document.getElementById('mio_id');
 
 nomi = ["Mario", "Luigi", "Francesco", "Giuseppe", "Franco", "Maria", "Francesca"];
 
+// numeri dallo 0 al 9
+var numeri = /[0-9]+$/;
+
+
 nomeUtente = prompt("Inserisci il tuo nome");
 
+// ciclo per la verifica della corrispondenza del nome
 for (var i = 0; i < nomi.length; i++) {
   if (nomeUtente === nomi[i]) {
     controllo = 1;
   }
 }
 
+// controllo presenza di numeri nella parola inserita dall'utente. null = nessun numero inserito
+var controlloNumeri = nomeUtente.match(numeri);
 
-if (controllo == 1) {
-  elemento.innerHTML = nomeUtente + " il tuo nome è presente nella lista, puoi partecipare alla festa.";
+
+// if per controllo della presenza di numeri
+if (controlloNumeri == null && nomeUtente != "") {
+  if (controllo == 1) {
+    elemento.innerHTML = nomeUtente + " il tuo nome è presente nella lista, puoi partecipare alla festa.";
+  } else {
+    elemento.innerHTML = nomeUtente + " il tuo nome NON è presente nella lista, NON puoi partecipare alla festa.";
+  }
 } else {
-  elemento.innerHTML = nomeUtente + " il tuo nome NON è presente nella lista, NON puoi partecipare alla festa.";
+  alert("Verifica di aver inserito i dati correttamente");
 }
